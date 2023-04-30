9e263fb394a3c27577711285cc1d977abfa689de
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-30 08:31:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,83 +1,70 @@
-//your JS code here. If required.
-const firstContainer = document.querySelector(".conatiner-1");
-const secondContainer = document.querySelector(".conatiner-2");
-const cells = document.querySelectorAll(".cell");
-const btn = document.getElementById("submit");
-const player1 = document.getElementById("player1");
-const player2 = document.getElementById("player2");
-const message = document.querySelector(".message");
-const xMark = "X";
-const oMark = "O";
-let turn = true;
-let win = false;
-const winnerCombo =[
-	[0,1,2],
-	[3,4,5],
-	[6,7,8],
-	[0,3,6],
-	[1,4,7],
-	[2,5,8],
-	[0,4,8],
-	[2,4,6]
-];
+let player1 = document.getElementById('player1');
+let player2 = document.getElementById('player2');
+// console.log(player2)
+let boxList = document.querySelectorAll('.box');
+let turn = 'x';
+let isGameOver = false;
+let form = document.querySelector('form');
 
-//to display tic tac toe playground
-btn.addEventListener("click",()=>{
-    if(player1.value!=="" && player2.value!==""){
-        firstContainer.style.display = "none";
-	    secondContainer.style.display = "block"
-        message.textContent = `${player1.value}, you're up`;
+
+//Function to change the turn
+const changeTurn = function () {
+  return turn === 'x' ? 'o' : 'x';
+};
+
+//Function to check for a win
+const checkWin = function () {
+  let boxTextList = document.querySelectorAll('.text');
+  let win = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  win.forEach((element) => {
+    if (
+      boxTextList[element[0]].innerText !== '' &&
+      boxTextList[element[0]].innerText === boxTextList[element[1]].innerText &&
+      boxTextList[element[1]].innerText === boxTextList[element[2]].innerText
+    ) {
+      isGameOver = true;
+      boxTextList[element[0]].parentNode.style.backgroundColor = 'purple';
+      boxTextList[element[1]].parentNode.style.backgroundColor = 'purple';
+      boxTextList[element[2]].parentNode.style.backgroundColor = 'purple';
     }
+  });
+};
+
+//Main Logic
+boxList.forEach((element) => {
+  element.addEventListener('click', function () {
+    let boxText = this.querySelector('.text');
+    if (boxText.innerText === '') {
+      boxText.innerText = turn;
+      turn = changeTurn();
+      checkWin();
+      if (isGameOver) {
+        const winner = turn === 'o' ? 'player1' : 'player2';
+        document.querySelector(
+          '.message'
+        ).innerText = `${winner}, congratulations you won!`;
+      } else {
+        const player = turn === 'x' ? 'player1' : 'player2';
+        document.querySelector('.message').innerText = `${player}, you\'re up`;
+      }
+    }
+  });
 });
 
-// iterating every cell
-cells.forEach(cell =>{
-	cell.addEventListener("click", handleClick, {once:true});
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.style.display = 'none';
+  document.querySelector('.game-container').style.display = 'flex';
+  let msg = document.querySelector('.message')
+  msg.innerText = `${player1.value}, you're up`
+  console.log(player2.value)
 });
-
-// every function that should happen on click
-function handleClick(e){
-	let currentCell = e.target;
-	const currentMark = turn? xMark : oMark;
-	markTicTac(currentCell,currentMark);
-	takeTurn();
-	if(winner(currentMark)){
-		win = true;
-		if(currentMark==="X"){
-			message.textContent = `${player1.value}, congratulations you won!`;
-		}
-		else{
-			message.textContent = `${player2.value}, congratulations you won!`;
-		}
-	}
-}
-
-// marking by different player 
-function markTicTac(currentCell,currentMark) {
-	if(currentMark==="X"){
-		message.textContent = `${player2.value}, you're up`;
-	}
-	else{
-		message.textContent = `${player1.value}, you're up`;
-	}
-
-	if(!win)
-		currentCell.textContent = currentMark;
-}
-
-//deciding winner
-function  winner(currentMark) {
-	return winnerCombo.some(combinations =>{
-		return combinations.every(element =>{
-			let val = cells[element].textContent;
-			if(val!==currentMark)
-				return false;
-			return true;
-		});
-	});
-}
-
-// alternating between player
-function  takeTurn() {
-	turn = !turn;
-}
